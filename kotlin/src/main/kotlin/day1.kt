73f5eedf94f2254ff07feb8bb3a0df4eca86dd13
Shu Wang <shu.wang@autotrader.co.uk>
2022-12-02 16:07:39 +0000

    Add day 1 solution

--- kotlin/src/main/kotlin/day1.kt
+++ kotlin/src/main/kotlin/day1.kt
@@ -11,13 +11,26 @@ fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> =
         acc
     }.filterNot { it.isEmpty() }
 
-//fun getTotalCalories(caloriesRecordByElf: List<List<String>>) {
-//    val totalCaloriesByElf = caloriesRecordByElf.map { it.sum() }
-//}
+fun getTotalCalories(caloriesRecordByElf: List<List<String>>): List<Int> {
+    val caloriesRecordByElfInt: List<List<Int>> = caloriesRecordByElf.map { it -> it.map { it.toInt() } }
+    return caloriesRecordByElfInt.map { it.sum() }
+}
+
+fun getMostTotalCalories(totalCaloriesByElf: List<Int>): Int {
+    return totalCaloriesByElf.sortedDescending()[0]
+}
+
+fun getTopThreeMostTotalCalories(totalCaloriesByElf: List<Int>): Int {
+    return totalCaloriesByElf.sortedDescending().take(3).sum()
+}
 
 fun main() {
-    val caloriesRecord: List<String> = readFile("calories_test.txt")
+    val caloriesRecord: List<String> = readFile("calories.txt")
     val caloriesRecordByElf: List<List<String>> = caloriesRecord.split { it.isBlank() }
-    println(caloriesRecord)
-    println(caloriesRecordByElf)
+    val totalCaloriesByElf = getTotalCalories(caloriesRecordByElf)
+    val mostCalories = getMostTotalCalories(totalCaloriesByElf)
+    println("most calories: $mostCalories")
+
+    val mostCaloriesTopThree = getTopThreeMostTotalCalories(totalCaloriesByElf)
+    println("most calories by top three elves: $mostCaloriesTopThree")
 }
