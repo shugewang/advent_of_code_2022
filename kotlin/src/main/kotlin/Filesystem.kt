64c83c9e822281711b3b1c62b8f8e1e8de8c1e57
Shu Wang <shugewang@hotmail.co.uk>
2023-01-06 17:00:43 +0000

    Add function to separate input into blocks of directories

--- kotlin/src/main/kotlin/Filesystem.kt
+++ kotlin/src/main/kotlin/Filesystem.kt
@@ -1,14 +1,40 @@
 import java.io.File
 
 class Filesystem (private val fileName: String) {
+    private val directories = extractDirectories()
     fun extractDirectories(): List<String> {
         var input =  File(fileName).readLines().filter { it.contains("cd") }.filterNot { it.contains("..") }
         return input.map { it.split(" ").takeLast(1).joinToString()}
     }
 
+    fun createListOfDirectoryClasses(): MutableList<Directory> {
 
-}
+        var directoriesList = mutableListOf<Directory>()
 
-class Item (var type: String, var size: Int) {
+        for (directory in directories) {
+            directoriesList.add(Directory(directory))
+        }
+        return directoriesList
+    }
+
+    fun separateIntoBlocksOfDirectories(): List<List<String>> {
+        var input = File(fileName).readLines().filterNot { it.contains("..") }
+        var blocks = mutableListOf(mutableListOf<String>())
+        for (item in input) {
+            if (item.contains("cd")) {
+                blocks.add(mutableListOf(item))
+            } else {
+                blocks[blocks.lastIndex].add(item)
+            }
+        }
+        return blocks.drop(1)
+    }
+
+    class Directory (var name: String){
+
+
+    }
+
+
 }
 
--- kotlin/src/test/kotlin/FilesystemTest.kt
+++ kotlin/src/test/kotlin/FilesystemTest.kt
@@ -9,11 +9,27 @@ class FilesystemTest {
         assertEquals(expected, filesystem.extractDirectories())
     }
 
-//    @Test
-//    fun createMapOfDirectoriesAndContentTest() {
-//        val expected = mapOf<Item, Item>()
-//        assertEquals(expected, filesystem.createDirectoriesMap())
-//    }
+    @Test
+    fun createListOfDirectoryClassesTest() {
+//        val expected = listOf(Filesystem.Directory("/"), Filesystem.Directory("a"), Filesystem.Directory("e"), Filesystem.Directory("d"))
+        val expected = listOf("/", "a", "e", "d")
+        val actual = mutableListOf<String>()
+        for (directory in filesystem.createListOfDirectoryClasses()) {
+            actual.add(directory.name)
+        }
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun separateBlockOfDirectoriesAndTheirContentsTest() {
+        val expected = listOf(listOf("$ cd /", "$ ls", "dir a", "14848514 b.txt", "8504156 c.dat", "dir d"), listOf("$ cd a", "$ ls", "dir e", "29116 f", "2557 g", "62596 h.lst"), listOf("$ cd e", "$ ls", "584 i"), listOf("$ cd d", "$ ls", "4060174 j", "8033020 d.log", "5626152 d.ext", "7214296 k"))
+        assertEquals(expected, filesystem.separateIntoBlocksOfDirectories())
+    }
+
+    @Test
+    fun createNewDirectoryTest() {
+
+    }
 
 
 }
