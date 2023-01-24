84bdddba361292fc1576aa3e344e25dfbd08cb15
Shu Wang <shugewang@hotmail.co.uk>
2023-01-24 14:20:25 +0000

    Add test for getting number of visible trees on the left

--- kotlin/src/test/kotlin/TreehouseTest.kt
+++ kotlin/src/test/kotlin/TreehouseTest.kt
@@ -79,6 +79,10 @@ class TreehouseTest {
         assertEquals(0, treehouse.getScenicScore(2, 4))
     }
 
-
+    @Test
+    fun getUnblockedViewToLeftTest() {
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
+        assertEquals(1, treehouse.getUnblockedReviewToLeft(1, 2))
+    }
 
 }
