2579bf8c21745aed87b1bb2b1eb03d158c9675b0
Shu Wang <shugewang@hotmail.co.uk>
2023-01-24 15:20:35 +0000

    Refactor by combining logics for each direction into getViewingDistance(), remove obsolete tests

--- kotlin/src/main/kotlin/Treehouse.kt
+++ kotlin/src/main/kotlin/Treehouse.kt
@@ -5,13 +5,13 @@ class Treehouse (private val fileName: String) {
     lateinit var treeMap: List<List<Int>>
     fun convertMapTo2DList(): List<List<Int>> {
         val input = File(fileName).readLines().map { it.split("").drop(1).dropLast(1) }
-        treeMap = input.map { it.map { it.toInt()} }
+        treeMap = input.map { it.map { it.toInt() } }
         return treeMap
     }
 
     fun countEdgeTrees(): Int {
-        val totalTrees = treeMap.size*treeMap[0].size
-        val edgeTrees = treeMap.size*2 + treeMap[0].size*2 - 4
+        val totalTrees = treeMap.size * treeMap[0].size
+        val edgeTrees = treeMap.size * 2 + treeMap[0].size * 2 - 4
         val innerTrees = totalTrees - edgeTrees
         return edgeTrees
     }
@@ -26,10 +26,10 @@ class Treehouse (private val fileName: String) {
     fun checkIfVisibleByCol(rowIndex: Int, colIndex: Int): Boolean {
         val tree = treeMap[rowIndex][colIndex]
         val aboveTrees = mutableListOf<Int>()
-        treeMap.take(rowIndex).map { aboveTrees.add(it[colIndex])}
+        treeMap.take(rowIndex).map { aboveTrees.add(it[colIndex]) }
 
         val belowTrees = mutableListOf<Int>()
-        treeMap.slice(rowIndex + 1 until treeMap.size).map { belowTrees.add(it[colIndex])}
+        treeMap.slice(rowIndex + 1 until treeMap.size).map { belowTrees.add(it[colIndex]) }
         return tree > aboveTrees.max() || tree > belowTrees.max()
     }
 
@@ -39,8 +39,8 @@ class Treehouse (private val fileName: String) {
 
     fun countNumberOfVisibleInnerTrees(): Int {
         var visibleInnerTrees = 0
-        for (row in 1..treeMap.size-2) {
-            for (col in 1.. treeMap[row].size-2) {
+        for (row in 1..treeMap.size - 2) {
+            for (col in 1..treeMap[row].size - 2) {
                 if (checkIfVisible(row, col)) {
                     visibleInnerTrees++
                 }
@@ -59,15 +59,16 @@ class Treehouse (private val fileName: String) {
         }
         return false
     }
+
     fun getScenicScore(rowIndex: Int, colIndex: Int): Int? {
         if (checkIfEdge(rowIndex, colIndex)) {
             return 0
         } else {
-            return getUnblockedViewToLeft(rowIndex, colIndex)*getUnblockedViewToRight(rowIndex, colIndex)*getUnblockedViewUp(rowIndex, colIndex)*getUnblockedViewDown(rowIndex, colIndex)
+            return getViewingDistance(rowIndex, colIndex).reduce { acc, i -> acc * i}
         }
     }
 
-    private fun getNumberOfVisibleTrees(trees: List<Int>, rowIndex: Int, colIndex: Int): Int{
+    private fun getNumberOfVisibleTrees(trees: List<Int>, rowIndex: Int, colIndex: Int): Int {
         var unblocked = 0
         for (tree in trees) {
             if (tree == treeMap[rowIndex][colIndex]) {
@@ -79,43 +80,23 @@ class Treehouse (private val fileName: String) {
         }
         return unblocked
     }
-    fun getUnblockedViewToLeft(rowIndex: Int, colIndex: Int): Int {
-        var visibleTreesCount = 0
-        if (!checkIfEdge(rowIndex, colIndex)) {
-            val leftTreesReversed = treeMap[rowIndex].slice(0 until colIndex).reversed()
-            visibleTreesCount = getNumberOfVisibleTrees(leftTreesReversed, rowIndex, colIndex)
-        }
+
+    private fun getViewingDistance(rowIndex: Int, colIndex: Int): List<Int> {
+        var visibleTreesCount = mutableListOf<Int>()
+        val leftTreesReversed = treeMap[rowIndex].slice(0 until colIndex).reversed()
+        visibleTreesCount.add(getNumberOfVisibleTrees(leftTreesReversed, rowIndex, colIndex))
+
+        val aboveTrees = mutableListOf<Int>()
+        treeMap.take(rowIndex).map { aboveTrees.add(it[colIndex]) }
+        visibleTreesCount.add(getNumberOfVisibleTrees(aboveTrees.reversed(), rowIndex, colIndex))
+
+        val rightTrees = treeMap[rowIndex].slice(colIndex + 1 until treeMap[colIndex].size)
+        visibleTreesCount.add(getNumberOfVisibleTrees(rightTrees, rowIndex, colIndex))
+
+        val belowTrees = mutableListOf<Int>()
+        treeMap.slice(rowIndex + 1 until treeMap.size).map { belowTrees.add(it[colIndex]) }
+        visibleTreesCount.add(getNumberOfVisibleTrees(belowTrees, rowIndex, colIndex))
+
         return visibleTreesCount
     }
-
-    fun getUnblockedViewToRight(rowIndex: Int, colIndex: Int): Int {
-        var visibleTreesCount = 0
-        if (!checkIfEdge(rowIndex, colIndex)) {
-            val rightTrees = treeMap[rowIndex].slice(colIndex + 1 until treeMap[colIndex].size)
-            visibleTreesCount = getNumberOfVisibleTrees(rightTrees, rowIndex, colIndex)
-        }
-        return visibleTreesCount
-    }
-
-    fun getUnblockedViewUp(rowIndex: Int, colIndex: Int): Int {
-        var visibleTreesCount = 0
-        if (!checkIfEdge(rowIndex, colIndex)) {
-            val aboveTrees = mutableListOf<Int>()
-            treeMap.take(rowIndex).map { aboveTrees.add(it[colIndex])}
-            visibleTreesCount = getNumberOfVisibleTrees(aboveTrees.reversed(), rowIndex, colIndex)
-        }
-        return visibleTreesCount
-    }
-
-    fun getUnblockedViewDown(rowIndex: Int, colIndex: Int): Int {
-        var visibleTreesCount = 0
-        if (!checkIfEdge(rowIndex, colIndex)) {
-            val belowTrees = mutableListOf<Int>()
-            treeMap.slice(rowIndex + 1 until treeMap.size).map { belowTrees.add(it[colIndex])}
-            visibleTreesCount = getNumberOfVisibleTrees(belowTrees, rowIndex, colIndex)
-        }
-        return visibleTreesCount
-    }
-
-
 }
