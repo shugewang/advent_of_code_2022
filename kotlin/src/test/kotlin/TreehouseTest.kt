b3b134f55e4c50487f7c780754b1d81f60201721
Shu Wang <shugewang@hotmail.co.uk>
2023-01-24 14:18:27 +0000

    Update tests to use example input instead of convertMapTo2DList()

--- kotlin/src/test/kotlin/TreehouseTest.kt
+++ kotlin/src/test/kotlin/TreehouseTest.kt
@@ -6,55 +6,56 @@ class TreehouseTest {
 
     @Test
     fun convertInputTo2DListTest() {
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         val expected = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(expected, treehouse.convertMapTo2DList())
     }
 
     @Test
     fun countNumberOfEdgeTrees() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(16, treehouse.countEdgeTrees())
     }
 
     @Test
     fun checkIfVisibleRowTrueTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(true, treehouse.checkIfVisibleByRow(2, 1))
     }
 
     @Test
     fun checkIfVisibleRowFalseTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(false, treehouse.checkIfVisibleByRow(2, 2))
     }
 
     @Test
     fun checkIfVisibleColTrueTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(true, treehouse.checkIfVisibleByCol(1, 1))
     }
 
     @Test
     fun checkIfVisibleColFalseTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(false, treehouse.checkIfVisibleByCol(3, 1))
     }
 
     @Test
     fun checkIfVisibleRowAndColTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(true, treehouse.checkIfVisible(1,2))
     }
 
     @Test
     fun countNumberOfVisibleInnerTreesTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(5, treehouse.countNumberOfVisibleInnerTrees())
     }
 
     @Test
     fun countNumberOfAllVisibleTreesTest() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         val innerTrees = treehouse.countNumberOfVisibleInnerTrees()
         val edgeTrees = treehouse.countEdgeTrees()
         assertEquals(21, treehouse.countNumberOfAllVisibleTrees(innerTrees, edgeTrees))
@@ -62,19 +63,22 @@ class TreehouseTest {
 
     @Test
     fun ifTopEdgeReturnZeroScenicScore() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(0, treehouse.getScenicScore(0, 0))
     }
 
     @Test
     fun ifBottomEdgeReturnZeroScenicScore() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(0, treehouse.getScenicScore(4, 2))
     }
 
     @Test
     fun ifRightEdgeReturnZeroScenicScore() {
-        treehouse.convertMapTo2DList()
+        treehouse.treeMap = listOf(listOf(3,0,3,7,3), listOf(2,5,5,1,2), listOf(6,5,3,3,2), listOf(3,3,5,4,9), listOf(3,5,3,9,0))
         assertEquals(0, treehouse.getScenicScore(2, 4))
     }
+
+
+
 }
